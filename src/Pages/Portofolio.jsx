f4b1e34ade599f28de7029c1902e2f2930254b95
Arun270647 <shankar72ind@gmail.com>
2025-06-09 19:32:56 +0530

    Fix StaticProjectCard import case mismatch

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -7,7 +7,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import StaticProjectCard from "../components/StaticProjectCard";
+import StaticProjectCard from "../components/staticprojectcard";
 import TechStackIcon from "../components/TechStackIcon";
 import AOS from "aos";
 import "aos/dist/aos.css";
